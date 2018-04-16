5b77c97116f1795f421106b33e0663953bbc030c
David Maldonado Garibay <david.maldonadogaribay@worktrucksolutions.com>
2018-04-15 22:03:43 -0700

    added models

--- server/server.js
+++ server/server.js
@@ -5,23 +5,47 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: 0
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-
-var newTodo = new Todo({
-  text: "cook dinner",
+var User = mongoose.model('User', {
+  email:{
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  }
 })
 
-newTodo.save().then((doc) => {
+
+// var newTodo = new Todo({
+//   text: "cook dinner",
+// })
+//
+// newTodo.save().then((doc) => {
+//   console.log(doc)
+// }, (e) => {
+//   console.log("unable to save todo")
+// })
+
+var newUser = new User({
+  email: "me@email.com"
+})
+
+newUser.save().then((doc) => {
   console.log(doc)
 }, (e) => {
-  console.log("unable to save todo")
+  console.log("unable to save user")
 })
